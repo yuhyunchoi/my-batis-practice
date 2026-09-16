1350f6d32857868cd781c94b8f3716d82fbd7daf
yuhyunchoi <dbgus315@gmail.com>
2026-09-16 13:55:58 +0900

    init commit

---
+++ src/test/java/com/yuhyun/mybatispractice/MbatispracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuhyun.mybatispractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbatispracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
